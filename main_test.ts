1dc2d8eaee07940a39a33e840520170d65223725
Ken Fowler <development@kennyf.ca>
2024-10-06 17:11:57 -0400

    feat: add tests

--- main_test.ts
+++ main_test.ts
@@ -1,6 +1,68 @@
-import { assertEquals } from "@std/assert";
-import { add } from "./main.ts";
+import { assertEquals } from '@std/assert';
+import { Scanner } from './scanner.ts';
+import { Parser } from './parser.ts';
 
-Deno.test(function addTest() {
-  assertEquals(add(2, 3), 5);
+Deno.test(function scanObject() {
+  const source = '{      }';
+  const scanner = new Scanner(source);
+  const tokens = scanner.scan();
+
+  const parser = new Parser(tokens);
+  const object = parser.parse();
+
+  assertEquals(object, {});
+});
+
+Deno.test(function scanArray() {
+  const source = '[   ]';
+  const scanner = new Scanner(source);
+  const tokens = scanner.scan();
+
+  const parser = new Parser(tokens);
+  const object = parser.parse();
+
+  assertEquals(object, []);
+});
+Deno.test(function scanKeyValue() {
+  const source = '{"key": "value"}';
+  const scanner = new Scanner(source);
+  const tokens = scanner.scan();
+
+  const parser = new Parser(tokens);
+  const object = parser.parse();
+
+  assertEquals(object, { key: 'value' });
+});
+
+Deno.test(function scanKeyValues() {
+  const source = '{ "key1": true, "key2": false, "key3": null, "key4": "value", "key5": 101.101 }';
+  const scanner = new Scanner(source);
+  const tokens = scanner.scan();
+
+  const parser = new Parser(tokens);
+  const object = parser.parse();
+
+  assertEquals(object, {
+    key1: true,
+    key2: false,
+    key3: null,
+    key4: 'value',
+    key5: 101.101,
+  });
+});
+
+Deno.test(function scanNestedKeyValues() {
+  const source = '{ "key": "value", "key-n": 101, "key-o": { "key-o-a": "nested" }, "key-l": [] }';
+  const scanner = new Scanner(source);
+  const tokens = scanner.scan();
+
+  const parser = new Parser(tokens);
+  const object = parser.parse();
+
+  assertEquals(object, {
+    key: 'value',
+    'key-n': 101,
+    'key-o': { 'key-o-a': 'nested' },
+    'key-l': [],
+  });
 });
