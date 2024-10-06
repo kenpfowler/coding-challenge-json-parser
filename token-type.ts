d3630c9277e14c9cddfd1faa0ac822d6aef4bf73
Ken Fowler <development@kennyf.ca>
2024-10-06 17:08:55 -0400

    feat: tokens

--- token-type.ts
+++ token-type.ts
@@ -1,1 +1,19 @@
-export enum TokenType {}
+export enum TokenType {
+  // single character tokens
+  LEFT_BRACKET = '[',
+  RIGHT_BRACKET = ']',
+  LEFT_BRACE = '{',
+  RIGHT_BRACE = '}',
+  COMMA = ',',
+  COLON = ':',
+
+  // literals
+  STRING = 'string',
+  NULL = 'null',
+  NUMBER = 'number',
+  TRUE = 'true',
+  FALSE = 'false',
+
+  // end of file
+  EOF = 'EOF',
+}
