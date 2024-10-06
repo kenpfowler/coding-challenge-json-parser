d9cda9d3bd6b5b0571d1a3066cd5f09066680ee4
Ken Fowler <development@kennyf.ca>
2024-10-06 17:09:02 -0400

    feat: token

--- token.ts
+++ token.ts
@@ -1,1 +1,24 @@
-class Token {}
+import { TokenType } from './token-type.ts';
+
+export class Token {
+  public readonly token_type;
+  public readonly line;
+  public readonly lexeme?;
+  public readonly literal?;
+
+  constructor(
+    token_type: TokenType,
+    line: number,
+    lexeme?: string,
+    literal?: null | boolean | string | number
+  ) {
+    this.lexeme = lexeme;
+    this.token_type = token_type;
+    this.line = line;
+    this.literal = literal;
+  }
+
+  public toString() {
+    return `[type: ${this.token_type}] line: ${this.line}, lexeme: ${this.lexeme}`;
+  }
+}
