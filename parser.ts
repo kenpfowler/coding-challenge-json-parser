4a3926f6e260a8c3a5e831fa05de9edbcbcb7eee
Ken Fowler <development@kennyf.ca>
2024-10-23 10:37:17 -0400

    refactor: advancement

--- parser.ts
+++ parser.ts
@@ -4,37 +4,39 @@ import { Token } from './token.ts';
 export class Parser {
   private readonly tokens;
   private current = 0;
+  private depth = 0;
+  private max_array_depth = 20;
+  private literals = [
+    TokenType.NULL,
+    TokenType.NUMBER,
+    TokenType.STRING,
+    TokenType.FALSE,
+    TokenType.TRUE,
+  ];
 
   constructor(tokens: Token[]) {
     this.tokens = tokens;
   }
 
   parse() {
-    return this.element();
+    const json = this.element();
+    this.consume(TokenType.EOF, 'expected EOF');
+    return json;
   }
 
   private element() {
-    while (!this.isAtEnd()) {
-      const token_type = this.advance()?.token_type;
+    const token = this.tokens[this.current];
+    const token_type = token.token_type;
 
-      switch (token_type) {
-        case TokenType.LEFT_BRACE:
-          return this.object();
-        case TokenType.LEFT_BRACKET:
-          return this.array();
-        default:
-          if (
-            token_type === TokenType.NULL ||
-            token_type === TokenType.STRING ||
-            token_type === TokenType.NUMBER ||
-            token_type === TokenType.TRUE ||
-            token_type === TokenType.FALSE
-          ) {
-            return this.value();
-          } else {
-            throw new Error(`invalid token of type: ${token_type}}`);
-          }
-      }
+    switch (token_type) {
+      case TokenType.LEFT_BRACE:
+        this.current++;
+        return this.object();
+      case TokenType.LEFT_BRACKET:
+        this.current++;
+        return this.array();
+      default:
+        throw new Error(`invalid token of type: ${token_type}}`);
     }
   }
 
@@ -44,93 +46,100 @@ export class Parser {
 
     switch (token_type) {
       case TokenType.LEFT_BRACE:
-        this.advance();
+        this.current++;
         return this.object();
       case TokenType.LEFT_BRACKET:
-        this.advance();
+        this.current++;
         return this.array();
       default:
-        if (
-          token_type === TokenType.NULL ||
-          token_type === TokenType.STRING ||
-          token_type === TokenType.NUMBER ||
-          token_type === TokenType.TRUE ||
-          token_type === TokenType.FALSE
-        ) {
+        if (this.literals.includes(token_type)) {
+          this.current++;
           return token.literal;
         } else {
-          throw new Error(`invalid json. found ${token}`);
+          throw new Error(`invalid json. found ${token} ${this.current}`);
         }
     }
   }
 
   private object() {
     const obj = new Object();
-    const current = this.tokens[this.current];
+    const token = this.tokens[this.current];
 
-    switch (current.token_type) {
+    switch (token.token_type) {
       case TokenType.RIGHT_BRACE:
+        this.current++;
         return obj;
       case TokenType.STRING:
-        return Object.assign(obj, this.member());
+        while (!this.isAtEnd()) {
+          const key = this.consume(TokenType.STRING, 'expected key');
+          this.consume(TokenType.COLON, 'expected colon');
+
+          Object.assign(obj, { [key.literal as string]: this.value() });
+
+          if (this.tokens[this.current].token_type === TokenType.RIGHT_BRACE) {
+            this.consume(TokenType.RIGHT_BRACE, 'expected right brace');
+            return obj;
+          }
+
+          this.consume(TokenType.COMMA, 'expected comma');
+        }
+
+        this.consume(TokenType.RIGHT_BRACE, 'expected right brace');
+        break;
       default:
-        throw new Error(`invalid json. found ${current}`);
+        throw new Error(`invalid json. found ${token}`);
     }
   }
 
   private array() {
-    const array: any = [];
-    const current = this.tokens[this.current];
+    this.depth++;
+    if (this.depth >= this.max_array_depth) {
+      throw new Error('array exceeds max depth');
+    }
 
-    switch (current.token_type) {
+    const array: any = [];
+    let token = this.tokens[this.current];
+
+    switch (token.token_type) {
       case TokenType.RIGHT_BRACKET:
+        this.depth--;
+        this.current++;
         return array;
       default:
-        array.push(this.value());
-        return array;
+        while (!this.isAtEnd()) {
+          array.push(this.value());
+
+          if (this.tokens[this.current].token_type === TokenType.RIGHT_BRACKET) {
+            this.consume(TokenType.RIGHT_BRACKET, 'expected right bracket');
+            this.depth--;
+            return array;
+          }
+
+          this.consume(TokenType.COMMA, `expected comma - found ${this.tokens[this.current]}`);
+        }
     }
-  }
-
-  private member(): any {
-    const members = [];
-    const key = this.consume(TokenType.COLON, 'expected colon');
-    this.advance();
-
-    const value = this.value();
-    members.push({ [key?.literal as string]: value });
-
-    if (this.check(TokenType.COMMA)) {
-      this.consume(TokenType.COMMA, 'expected comma');
-      this.consume(TokenType.STRING, 'expected string');
-      members.push(this.member());
-    }
-
-    this.advance();
-    return members.reduce((accumulator, currentValue) => ({ ...accumulator, ...currentValue }), {});
   }
 
   // helper functions
-
-  private advance() {
-    return this.tokens.at(this.current++);
-  }
-
   private isAtEnd() {
-    if (this.peek()?.token_type === TokenType.EOF) return true;
-
+    if (this.tokens[this.current].token_type === TokenType.EOF) {
+      return true;
+    }
     return this.current >= this.tokens.length;
   }
 
   private peek() {
-    return this.tokens.at(this.current + 1);
+    return this.tokens[this.current + 1];
   }
 
   private check(token_type: TokenType) {
-    return this.peek()?.token_type === token_type;
+    return this.tokens[this.current].token_type === token_type;
   }
 
   private consume(type: TokenType, message: string) {
-    if (this.check(type)) return this.advance();
+    if (this.check(type)) {
+      return this.tokens[this.current++];
+    }
 
     throw new Error(message);
   }
